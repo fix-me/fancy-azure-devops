3caf5da85ee5c28d85418f7b6b7d934fe55c6540
fix-me <43244072+fix-me@users.noreply.github.com>
2021-01-20 18:05:07 +0100

    update styling periodically within animation frame , V. 0.2

--- taskboard.user.js
+++ taskboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Azure DevOps Fancy Taskboard
 // @namespace    https://github.com/fix-me/
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/fix-me/fancy-azure-devops/master/taskboard.user.js
 // @downloadURL  https://raw.githubusercontent.com/fix-me/fancy-azure-devops/master/taskboard.user.js
 // @description  On the Azure DevOps taskboard, highlight tasks with tags 'AC', 'frontend', 'backend', 'machine', 'devops'
@@ -18,19 +18,33 @@
     const isTask = node => node.parentNode.getAttribute("aria-label").indexOf("Task") === 0;
 
     const styleByTag = (tag, color) => {
-        Array.from(document.getElementsByClassName("tag-box"))
-            .filter(t=>t.innerHTML.toLowerCase()===tag)
-            .map(t=>t.closest(".tbTileContent"))
-            .filter(isTask)
-            .forEach(t=>{t.style="background-color: " + color})
+        try {
+            Array.from(document.getElementsByClassName("tag-box"))
+                .filter(t=>t.innerHTML.toLowerCase()===tag)
+                .map(t=>t.closest(".tbTileContent"))
+                .filter(isTask)
+                .forEach(t=>{t.style="background-color: " + color})
+        } catch(e) {
+            // No matching element to style, probably opened item details
+        }
     };
 
-    setTimeout(()=>{
+    const doStyle = () => {
         styleByTag("ac", "Khaki");
         styleByTag("frontend", "PaleGreen");
         styleByTag("backend", "MistyRose");
         styleByTag("machine", "LightCyan");
         styleByTag("devops", "Gainsboro");
-    },3000);
+    };
 
+    const update = () => {
+        requestAnimationFrame(()=>{
+            doStyle();
+            setTimeout(()=>{
+                update();
+            }, 5000);
+        });
+    };
+
+    update();
 })();
